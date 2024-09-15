44957eb373d4bb3e4e41b08c5fe9ac97247643c2
Emmanuel Opare <emmanuelopare763@gmail.com>
2024-09-15 21:16:36 +0000

    done

--- recipe-sharing-platform/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  media: false,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
