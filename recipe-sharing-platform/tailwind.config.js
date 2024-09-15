97658bec0bbf99b3628dc6f64f6390ed36647904
Emmanuel Opare <emmanuelopare763@gmail.com>
2024-09-15 20:50:21 +0000

    Add initial files

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  media: false,
   theme: {
     extend: {},
   },
